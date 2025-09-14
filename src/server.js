fe31ee83416137cd876fa0473c61785e2faea893
Utarasetyaw <tarazekage@gmail.com>
2025-09-14 18:42:14 +0700

    fix: update default server port from 5000 to 3009

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ import dashboardRoutes from './routes/dashboard.routes.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3009;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
